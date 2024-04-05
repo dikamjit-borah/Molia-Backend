4728e4d6f6599348939d57cb3cb6c72e23183dcc
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2024-04-05 06:03:19 +0530

    feat. custom lists

--- services/service.js
+++ services/service.js
@@ -1,11 +1,24 @@
+/* eslint-disable multiline-ternary */
 const { insertOne, findOne, findOneAndUpdate } = require("../db/mongo.handler");
+const { collections } = require("../utils/app.constant");
 
 const saveTitle = async (data) => {
   try {
-    const collection = data.entry_type;
-    const user_id = data.user_id;
-    const details = data.details;
+    const { user_id, collection, sub_collection, details } = { ...data };
+
     const document = await findOne(collection, { user_id });
+
+    if (collection === collections.custom) {
+      return saveForCustomList(
+        document,
+        user_id,
+        collection,
+        sub_collection,
+        details
+      );
+    }
+
+    //save for pre-defined lists
     if (!document) {
       return await insertOne(collection, { user_id, titles: [details] });
     }
@@ -21,6 +34,35 @@ const saveTitle = async (data) => {
     console.log(`Error saving title for ${data.user_id}: ${error}`);
     throw error;
   }
+};
+
+const saveForCustomList = async (
+  document,
+  user_id,
+  collection,
+  sub_collection,
+  details
+) => {
+  if (!document) {
+    const data = { user_id };
+    data[sub_collection] = { titles: [details] };
+    return await insertOne(collection, data);
+  }
+  // Check if the sub_collection exists in the document
+  if (!document[sub_collection]) {
+    // If not, create it with the provided details
+    document[sub_collection] = { titles: [details] };
+  } else {
+    // If it exists, push the details to the titles array
+    document[sub_collection].titles.push(details);
+  }
+
+  // Update the document in the database
+  return await findOneAndUpdate(
+    collection,
+    { user_id },
+    { $set: { [sub_collection]: document[sub_collection] } }
+  );
 };
 
 const fetchTitles = async (data) => {
