6de4f979c6fff06783a3187010d58c0117431541
dikamjit-flexiloans <dikamjit.borah@flexiloans.com>
2024-04-05 14:49:27 +0530

    hf. add error middleware in payload validation

--- middlewares/validate.middleware.js
+++ middlewares/validate.middleware.js
@@ -1,18 +1,26 @@
-const { sendError, validateWithJoi } = require("../utils/app.helpers");
+const NE = require("node-exceptions");
+const { validateWithJoi } = require("../utils/app.helpers");
 
 module.exports = {
   validateRequest: (schema) => {
     return (req, res, next) => {
-      const { error } = validateWithJoi(schema, {
-        ...req.params,
-        ...req.query,
-        ...req.body,
-      });
-      const valid = error == null;
-      if (!valid) {
-        return sendError(res, 400, "Bad Request", error.details[0]);
-      } else {
-        next();
+      try {
+        const { error } = validateWithJoi(schema, {
+          ...req.params,
+          ...req.query,
+          ...req.body,
+        });
+        const valid = error == null;
+        if (!valid) {
+          throw new NE.InvalidArgumentException(
+            `Bad Request ${error.details[0].message}`,
+            400
+          );
+        } else {
+          next();
+        }
+      } catch (error) {
+        next(error);
       }
     };
   },
