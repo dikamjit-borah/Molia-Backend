879c34789ea2c7cf68b576ddd919396ad26b8028
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2024-04-05 06:18:43 +0530

    controller changes

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,19 +1,27 @@
+/* eslint-disable nonblock-statement-body-position */
 const NE = require("node-exceptions");
 const { saveTitle, fetchTitles } = require("../services/service");
-const { sendResponse, sendError, createJson } = require("../utils/app.helpers");
+const { createJson } = require("../utils/app.helpers");
 const { messages } = require("../utils/app.constant");
 
-const save = async (req, res) => {
-  res.send(await saveTitle(req.body));
+const save = async (req, res, next) => {
+  try {
+    await saveTitle(req.body);
+    res.json(
+      createJson(201, `Successfully inserted to ${req.body.collection} list`)
+    );
+  } catch (error) {
+    next(error);
+  }
 };
 
 const titles = async (req, res, next) => {
   try {
-    const { user_id, entry_type } = { ...req.params, ...req.query };
-    const titles = await fetchTitles({ user_id, entry_type });
+    const { user_id, collection } = { ...req.params, ...req.query };
+    const titles = await fetchTitles({ user_id, collection });
     if (titles && titles.length)
       return res.json(
-        createJson(200, `Successfully fetched ${entry_type} titles`, titles)
+        createJson(200, `Successfully fetched ${collection} list`, titles)
       );
     throw new NE.LogicalException(messages.not_found, 404);
   } catch (error) {
